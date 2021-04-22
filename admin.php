4800fd2499b46aeb286addb149b5b9bf33af7e49
Cezar Leigos <cezarleigos28@outlook.com>
2021-04-22 20:01:06 -0300

    v0.03 - Admin pages
    This time, we have created some account administration pages, these pages will be useful in the future, when we start creating the user panel.

--- admin.php
+++ admin.php
@@ -2,6 +2,12 @@
 <html>
 <head>
 	<title>Direct Selling</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name='robots' content='noindex,follow' />
+	<link rel="sortcut icon" href="app/system/images/icon_radius.png" type="image/x-icon" />
+	<link rel="stylesheet" type="text/css" href="app/system/css/style_mobile.css">
+	<link rel="stylesheet" type="text/css" href="app/system/css/style_desktop.css">
 	<?php include("app/php/colors/theme-color.php"); ?>
 </head>
 <body>
