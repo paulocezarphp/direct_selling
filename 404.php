00fb3900d2b28563beb5e369fede511909bb33a5
Cezar Leigos <cezarleigos28@outlook.com>
2021-04-24 13:42:57 -0300

    v0.05 - Creating error page
    This time, we started to create an error page that will appear whenever a page on the site does not exist.

---
+++ 404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page not found</title>
+	<link rel="stylesheet" type="text/css" href="app/system/css/404.css">
+</head>
+<body>
+
+	<div class="a404">
+		<div class="a404-item">
+			<div class="a404-h1">404</div>	
+			<div class="a404-message">Page not found</div>
+            <div class="a404-back"><a href="./" class="a404-link">back to site</div>
+		</div>	
+	</div>	
+
+</body>
+</html>
